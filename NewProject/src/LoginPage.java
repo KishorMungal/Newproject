94d323e4e2d70158e0e02c98ffc51db1c433be63
KishorMungal <kishormungal1997@gmail.com>
2021-09-09 09:03:47 +0530

    https://github.com/KishorMungal/Newproject.git
    hey ,i have done some  changes in login page

--- NewProject/src/LoginPage.java
+++ NewProject/src/LoginPage.java
@@ -3,6 +3,10 @@ public class LoginPage {
 
 	public static void main(String[] args) {
     System.out.println("this is a login page");
+	
+	System.out.println("second change in login page");
+	
 	}
+	  
 
 }
